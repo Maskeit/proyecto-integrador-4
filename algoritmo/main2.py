0fd430a73990f68d0932a672f4b926cd561af17e
[Xime Mzo] <54044787+ximenamzo@users.noreply.github.com>
2023-04-17 19:50:58 -0600

    Se agrega en main funcion para enviar a db

--- algoritmo/main2.py
+++ algoritmo/main2.py
@@ -1,45 +1,27 @@
 import torch
 import cv2
 import numpy as np
+
+#Bloque para la funcion de enviar el dato a otro archivo
 import socket
-import psycopg2
 
 def enviarData(num_persona):
-    # Conectar la base de datos
-    conn = psycopg2.connect(
-        host="smart-crowd.postgres.database.azure.com",
-        database="smart-crowd",
-        user="django@smart-crowd",
-        password="MAdj2023!*",
-        sslmode="require"
-    )
-
-    # Crear cursor
-    cursor = conn.cursor()
-    # Insertar los valores en la tabla
-    #consulta = f"INSERT INTO nombre_tabla (columna_resultado) VALUES ({operacion})"
-    cursor.execute("INSERT INTO public.\"aforoReg_register\" (id_place, id_cam, personas) VALUES (%s, %s, %s)", (8, 1, num_persona))
-    conn.commit()
-    # Cerrar la conexión
-    conn.close()
-
-
-    # # Crear socket y conectar con el servidor
-    # HOST = 'localhost'  # Host del servidor
-    # PORT = 18748  # Puerto utilizado por el servidor
-    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    #     s.connect((HOST, PORT))
-    #     # Enviar el número de personas detectadas como una cadena de texto
-    #     s.sendall(str(num_persona).encode())
-    #     # Esperar respuesta del servidor (opcional)
-    #     data = s.recv(1024)
-    #     print(f"Respuesta del servidor: {data.decode()}")
+    # Crear socket y conectar con el servidor
+    HOST = 'localhost'  # Host del servidor
+    PORT = 18748  # Puerto utilizado por el servidor
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        # Enviar el número de personas detectadas como una cadena de texto
+        s.sendall(str(num_persona).encode())
+        # Esperar respuesta del servidor (opcional)
+        data = s.recv(1024)
+        print(f"Respuesta del servidor: {data.decode()}")
 #
 
 #leemos el modelo
 model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'C:/Users/ximen/PycharmProjects/pythonProject/proyectoOpenCV/yolo/proyecto-integrador-4/algoritmo/bestv5.pt' )
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(2)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 # Aumentar el brillo multiplicando los valores de los pixeles
--- algoritmo/receiveData.py
+++ algoritmo/receiveData.py
@@ -4,31 +4,12 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import datetime
 
-def send_email(subject, body):
-    sender_email = "miguelagustin182@gmail.com"  # Replace with your email address
-    receiver_email = "malejandre@ucol.mx"  # Replace with recipient email address
-    password = "contrasena_ejem"  # Replace with your email password
-
-    message = MIMEMultipart()
-    message["Subject"] = subject
-    message["From"] = sender_email
-    message["To"] = receiver_email
-
-    # Add body to email
-    message.attach(MIMEText(body, "plain"))
-
-    # Create secure connection with server and send email
-    context = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    context.login(sender_email, password)
-    context.sendmail(sender_email, receiver_email, message.as_string())
-    context.quit()
-
 def getData():
     # crea un objeto socket
     s = socket.socket()
     # especifica la dirección IP y el puerto en los que el servidor va a escuchar
     host = 'localhost'
-    port = 12345
+    port = 18748
     # enlazar el socket al servidor
     s.bind((host, port))
     # escucha conexiones entrantes
@@ -45,9 +26,9 @@ def getData():
 
 # llama a la función getData para recibir los datos
 while True:
-    try:
-        getData()
-    except Exception as e:
-        error_msg = f"Se produjo un error en el servidor: {e}"
-        print(error_msg)
-        send_email("error en el servidor", "Reparar urgentemente o reconectar servidor")
+    #try:
+    getData()
+    # except Exception as e:
+    #     error_msg = f"Se produjo un error en el servidor: {e}"
+    #     print(error_msg)
+    #     send_email("error en el servidor", "Reparar urgentemente o reconectar servidor")
